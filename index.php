a938838c36fc709fa3b49c063c3b00feb06aaf65
vntrieu <vntrieu25879@gmail.com>
2021-03-03 10:01:18 +0700

    test

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Bai 5</title>
-</head>
-<body>
-	<h1>Vo Nhat Trieu</h1>
-	<!-- <form method="GET">
-		<label>Ho ten</label>
-		<input type="text" name="hoten">
-		<br>
-		<label>Email</label>
-		<input type="text" name="email">
-		<input type="submit" name="submit" value="submit">
-	</form>
-	<?php 
-		echo "Chao ban $GET['name'], email cua ban la: $GET['email']";
-	 ?> -->
-</body>
-</html>
+<?php 
+echo "Hi"; 
+?>
