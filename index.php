7cd8a9f96248c9c11c63a04bd3f8ad50c603a3a3
vntrieu <vntrieu25879@gmail.com>
2021-03-03 09:47:50 +0700

    add form

--- index.php
+++ index.php
@@ -5,5 +5,16 @@
 </head>
 <body>
 	<h1>Vo Nhat Trieu</h1>
+	<form method="GET">
+		<label>Ho ten</label>
+		<input type="text" name="hoten">
+		<br>
+		<label>Email</label>
+		<input type="text" name="email">
+		<input type="submit" name="submit" value="submit">
+	</form>
+	<?php 
+		echo "Chao ban $GET['name'], email cua ban la: $GET['email']";
+	 ?>
 </body>
 </html>
