64ac9cee905dcf934bc5c00c66172930046c02bc
Nghia <nghianguyen288799@gmail.com>
2021-03-31 14:29:54 +0700

    add php pages

---
+++ add_acount.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Add count</title>
+	
+
+</head>
+<body>
+	<form method="POST">
+		<table>
+			<tr>
+				<td>Username</td>
+				<td>
+					<input type="text" name="username">		
+				</td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td>
+					<input type="text" name="password">
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<input type="submit" value="submit">
+				</td>
+			</tr>
+		</table>
+	</form>
+
+</body>
+</html>
+<?php 
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	$db = pg_connect(pg_connection_string_from_database_url() );
+   if(!$db){
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
+   
+   $sql =<<<EOF
+      INSERT INTO my_accounts (USERNAME, PASSWORD) VALUES ('$username', '$password');
+EOF;
+
+   $ret = pg_query($db, $sql);
+   if(!$ret){
+      echo pg_last_error($db);
+   } else {
+      echo "Table created successfully\n";
+   }
+   pg_close($db);
+?>
---
+++ setup.php
@@ -0,0 +1,27 @@
+<?php
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+   $db = pg_connect(pg_connection_string_from_database_url() );
+   if(!$db){
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
+   
+   $sql =<<<EOF
+      CREATE TABLE my_accounts
+      (
+      USERNAME           CHAR(10)    NOT NULL PRIMARY KEY,
+      PASSWORD           CHAR(50)     NOT NULL);
+EOF;
+
+   $ret = pg_query($db, $sql);
+   if(!$ret){
+      echo pg_last_error($db);
+   } else {
+      echo "Table created successfully\n";
+   }
+   pg_close($db);
+?>
