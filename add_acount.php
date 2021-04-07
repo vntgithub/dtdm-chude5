20eabac37b5cb301a84b6b10f2fe2a3d72de9af1
Nhu Y <kimhyejinst01259@gmail.com>
2021-04-07 14:25:46 +0700

    modify

--- add_acount.php
+++ add_acount.php
@@ -6,7 +6,7 @@
 
 </head>
 <body>
-	<form method="POST">
+	<form method="POST" action="">
 		<table>
 			<tr>
 				<td>Username</td>
@@ -27,29 +27,29 @@
 			</tr>
 		</table>
 	</form>
+	<?php 
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+
+		$db = pg_connect(pg_connection_string_from_database_url() );
+	   if(!$db){
+	      echo "Error : Unable to open database\n";
+	   } else {
+	      echo "Opened database successfully\n";
+	   }
+	   
+	   $sql =<<<EOF
+	      INSERT INTO my_accounts (USERNAME, PASSWORD) VALUES ('{$username}', '{$password}');
+	EOF;
+
+	   $ret = pg_query($db, $sql);
+	   if(!$ret){
+	      echo pg_last_error($db);
+	   } else {
+	      echo "Add user successfully\n";
+	   }
+	   pg_close($db);
+	?>
 
 </body>
 </html>
-<?php 
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-
-	$db = pg_connect(pg_connection_string_from_database_url() );
-   if(!$db){
-      echo "Error : Unable to open database\n";
-   } else {
-      echo "Opened database successfully\n";
-   }
-   
-   $sql =<<<EOF
-      INSERT INTO my_accounts (USERNAME, PASSWORD) VALUES ('$username', '$password');
-EOF;
-
-   $ret = pg_query($db, $sql);
-   if(!$ret){
-      echo pg_last_error($db);
-   } else {
-      echo "Add user successfully\n";
-   }
-   pg_close($db);
-?>
